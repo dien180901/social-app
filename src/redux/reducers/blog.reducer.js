a00853e73e00163038268527f49b9ac29a75e47a
Hai Ma <haima@Hais-MacBook-Pro.local>
2020-10-04 21:08:25 +0700

    Merge

--- src/redux/reducers/blog.reducer.js
+++ src/redux/reducers/blog.reducer.js
@@ -25,48 +25,8 @@ const blogReducer = (state = initialState, action) => {
     case types.GET_SINGLE_BLOG_REQUEST_FAILURE:
       return { ...state, loading: false };
 
-// }
-// const blogReducer=(state=initialState,action)=>{
-//     const {types,payload}=action;
-//     switch(types){
-//         case types.GET_BLOGS_REQUEST:
-//             return {...state,loading:true};
-//         case types.GET_BLOGS_SUCCESS:
-//             return {...state,blogs:payload.blogs,
-//             totalPageNum:payload.totalPages,
-//                 isLoading:false,
-//         }
-
-//         default:
-//             return state;
-//     }
-// }
-// export default blogReducer;
-import * as types from "redux/constants/blog.constants";
-const initialState = {
-  blogs: [],
-  totalPageNum: 1,
-  selectedBlog: null,
-  loading: false,
+    default:
+      return state;
+  }
 };
-
-const blogReducer = (state = initialState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-      case types.GET_BLOGS_REQUEST:
-        return { ...state, loading: true };
-      case types.GET_BLOGS_SUCCESS:
-        return {
-          ...state,
-          blogs: payload.blogs,
-          totalPageNum: payload.totalPages,
-          loading: false,
-        };
-      case types.GET_BLOGS_FAILURE:
-        return { ...state, loading: false };
-      default:
-        return state;
-    }
-  };
-  
-  export default blogReducer;
+export default blogReducer;
