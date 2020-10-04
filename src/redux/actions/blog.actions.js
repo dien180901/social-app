a24027da58115ad81cf08e7775052a1e021ecca1
Minh <dminhnguyen161@gmail.com>
2020-10-04 21:18:46 +0700

    blog detail

--- src/redux/actions/blog.actions.js
+++ src/redux/actions/blog.actions.js
@@ -7,9 +7,7 @@
 //     try{
 //         const res=api.get(`/blogs?page=${pageNum}&limit=${limit}`)
 //         dispatch({type:types.GET_BLOGS_SUCCESS,payload:res.data.data})
-    
-    
-    
+
 //     }catch(error){
 //         dispatch({type:types.GET_BLOGS_FAILURE,payload:error});
 
@@ -22,16 +20,30 @@ import * as types from "redux/constants/blog.constants";
 import api from "../reducers/api";
 import { toast } from "react-toastify";
 const blogsRequest = (pageNum = 1, limit = 10) => async (dispatch) => {
-    dispatch({ type: types.GET_BLOGS_REQUEST, payload: null });
-    try {
-      const res = await api.get(`/blogs?page=${pageNum}&limit=${limit}`);
-      dispatch({ type: types.GET_BLOGS_SUCCESS, payload: res.data.data });
-    } catch (error) {
-      console.log(error)
-      dispatch({ type: types.GET_BLOGS_FAILURE, payload: error });
-    }
-  };
-  
-  export const blogActions = {
-    blogsRequest,
-  };
+  dispatch({ type: types.GET_BLOGS_REQUEST, payload: null });
+  try {
+    const res = await api.get(`/blogs?page=${pageNum}&limit=${limit}`);
+    dispatch({ type: types.GET_BLOGS_SUCCESS, payload: res.data.data });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: types.GET_BLOGS_FAILURE, payload: error });
+  }
+};
+
+const getSingleBlog = (blogId) => async (dispatch) => {
+  dispatch({ type: types.GET_SINGLE_BLOG_REQUEST, payload: null });
+  try {
+    const res = await api.get(`/blogs/${blogId}`);
+    dispatch({
+      type: types.GET_SINGLE_BLOG_REQUEST_SUCCESS,
+      payload: res.data.data
+    });
+  } catch (error) {
+    dispatch({ type: types.GET_SINGLE_BLOG_REQUEST_FAILURE, payload: error });
+  }
+};
+
+export const blogActions = {
+  blogsRequest,
+  getSingleBlog
+};
--- src/redux/reducers/blog.reducer.js
+++ src/redux/reducers/blog.reducer.js
@@ -28,26 +28,32 @@ const initialState = {
   blogs: [],
   totalPageNum: 1,
   selectedBlog: null,
-  loading: false,
+  loading: false
 };
 
 const blogReducer = (state = initialState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-      case types.GET_BLOGS_REQUEST:
-        return { ...state, loading: true };
-      case types.GET_BLOGS_SUCCESS:
-        return {
-          ...state,
-          blogs: payload.blogs,
-          totalPageNum: payload.totalPages,
-          loading: false,
-        };
-      case types.GET_BLOGS_FAILURE:
-        return { ...state, loading: false };
-      default:
-        return state;
-    }
-  };
-  
-  export default blogReducer;
+  const { type, payload } = action;
+  switch (type) {
+    case types.GET_SINGLE_BLOG_REQUEST:
+      return { ...state, loading: true };
+    case types.GET_SINGLE_BLOG_REQUEST_SUCCESS:
+      return { ...state, selectedBlog: payload, loading: false };
+    case types.GET_SINGLE_BLOG_REQUEST_FAILURE:
+      return { ...state, loading: false };
+    case types.GET_BLOGS_REQUEST:
+      return { ...state, loading: true };
+    case types.GET_BLOGS_SUCCESS:
+      return {
+        ...state,
+        blogs: payload.blogs,
+        totalPageNum: payload.totalPages,
+        loading: false
+      };
+    case types.GET_BLOGS_FAILURE:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+};
+
+export default blogReducer;
